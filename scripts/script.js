06027239027f4a204814f110725aca07854d329e
Bogdan <buselb2@gmail.com>
2021-01-20 22:30:54 +0200

    fetch data

--- scripts/script.js
+++ scripts/script.js
@@ -1,9 +1,32 @@
+class FetchData {
+ getResourse = async url => {
+  const res = await fetch(url);
+
+  if (!res.ok) {
+   throw new Error('Произошда ошибка :' + res.status);
+  }
+  
+  return res.json();
+ }
+
+ getPost = () => this.getResourse('db/database.json');
+ 
+}
+
 class Twitter {
  constructor({ listElem }) {
+  const fetchData = new FetchData();
   this.tweets = new Posts();
   this.elements = {
    listElem: document.querySelector(listElem),
   }
+
+  fetchData.getPost()
+   .then(data => {
+    data.forEach(this.tweets.addPost);
+    
+   });
+   console.log('this.tweets: ', this.tweets);
  }
 
  renderPosts() {
@@ -32,9 +55,8 @@ class Posts {
   this.posts = posts;
  }
 
- addPost(tweet) {
-  const post = new Post(tweet);
-  this.posts.push(post);
+ addPost = (tweets) => {
+  this.posts.push(tweets);
  }
 
  deletePost(id) {
@@ -47,14 +69,14 @@ class Posts {
 }
 
 class Post {
- constructor(param) {
-  this.id = param.id;
-  this.userName = param.userName;
-  this.nickName = param.nickName;
-  this.postDate = param.postDate;
-  this.text = param.text;
-  this.img = param.img;
-  this.likes = param.likes;
+ constructor({ id, userName, nickName, postDate, text, img, likes = 0 }) {
+  this.id = id || this.generateID();
+  this.userName = userName;
+  this.nickName = nickName;
+  this.postDate = postDate;
+  this.text = text;
+  this.img = img;
+  this.likes = likes;
   this.liked = false;
  }
 
@@ -66,10 +88,24 @@ class Post {
    this.likes--;
   }
  }
+
+ generateID() {
+  return Math.random().toString(32).substring(2, 9) + (+new Date).toString(32);
+ }
+
+ getDate() {
+  const options = {
+   year: 'numeric',
+   month: 'numeric',
+   day: 'numeric',
+   hour: '2-digit',
+   minute: '2-digit',
+  }
+  return this.postDate.toLocaleString('ru-RU', options);
+ }
 }
 
 const twitter = new Twitter({
  listElem: '.tweet-list'
-})
+});
 
-console.log('twitter: ', twitter);
