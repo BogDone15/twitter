b0499d2697ece4a546f0846f981b093adff0c945
Bogdan <buselb2@gmail.com>
2021-01-21 22:03:43 +0200

    render list items

--- scripts/script.js
+++ scripts/script.js
@@ -1,111 +1,151 @@
 class FetchData {
- getResourse = async url => {
-  const res = await fetch(url);
+  getResourse = async url => {
+    const res = await fetch(url);
 
-  if (!res.ok) {
-   throw new Error('Произошда ошибка :' + res.status);
+    if (!res.ok) {
+      throw new Error('Произошла ошибка :' + res.status);
+    }
+
+    return res.json();
   }
-  
-  return res.json();
- }
 
- getPost = () => this.getResourse('db/database.json');
- 
+  getPost = () => this.getResourse('db/database.json');
+
 }
 
 class Twitter {
- constructor({ listElem }) {
-  const fetchData = new FetchData();
-  this.tweets = new Posts();
-  this.elements = {
-   listElem: document.querySelector(listElem),
+  constructor({ listElem }) {
+    const fetchData = new FetchData();
+    this.tweets = new Posts();
+    this.elements = {
+      listElem: document.querySelector(listElem),
+    }
+
+    fetchData.getPost()
+      .then(data => {
+        data.forEach(this.tweets.addPost);
+        this.showAllPost();
+      });
+
   }
 
-  fetchData.getPost()
-   .then(data => {
-    data.forEach(this.tweets.addPost);
-    
-   });
-   console.log('this.tweets: ', this.tweets);
- }
+  renderPosts(tweets) {
+    this.elements.listElem.textContent = '';
 
- renderPosts() {
+    tweets.forEach(({ id, userName, nickname, text, img, likes, getDate }) => {
+      this.elements.listElem.insertAdjacentHTML('beforeend', `
+      <li>
+        <article class="tweet">
+          <div class="row">
+            <img class="avatar" src="images/${nickname}.jpg" alt="Аватар пользователя ${nickname}">
+            <div class="tweet__wrapper">
+              <header class="tweet__header">
+                <h3 class="tweet-author">${userName}
+                  <span class="tweet-author__add tweet-author__nickname">@${nickname}</span>
+                  <time class="tweet-author__add tweet__date">${getDate()}</time>
+                </h3>
+                <button class="tweet__delete-button chest-icon" data-id="${id}"></button>
+              </header>
+              <div class="tweet-post">
+                <p class="tweet-post__text">${text}</p>
+               
+                  ${
+                    img ?
+                    `<figure class="tweet-post__image">
+                  <img src="${img}" alt="Сообщение ${nickname} Lorem ipsum dolor sit amet, consectetur.">
+                  </figure>` :
+                  ''
+                
+                }
+                  
+              
+              </div>
+            </div>
+          </div>
+          <footer>
+            <button class="tweet__like">
+              ${likes}
+            </button>
+          </footer>
+        </article>
+      </li>
+    `);
+    })
+  }
 
- }
+  showUserPost() {
 
- showUserPost() {
+  }
 
- }
+  showLikesPost() {
 
- showLikesPost() {
+  }
 
- }
+  showAllPost() {
+    this.renderPosts(this.tweets.posts);
+  }
 
- showAllPost() {
+  openModal() {
 
- }
-
- openModal() {
-
- }
+  }
 }
 
 class Posts {
- constructor({ posts = [] } = {}) {
-  this.posts = posts;
- }
+  constructor({ posts = [] } = {}) {
+    this.posts = posts;
+  }
 
- addPost = (tweets) => {
-  this.posts.push(tweets);
- }
+  addPost = (tweets) => {
+    this.posts.push(new Post(tweets));
+  }
 
- deletePost(id) {
+  deletePost(id) {
 
- }
+  }
 
- likedPost(id) {
+  likedPost(id) {
 
- }
+  }
 }
 
 class Post {
- constructor({ id, userName, nickName, postDate, text, img, likes = 0 }) {
-  this.id = id || this.generateID();
-  this.userName = userName;
-  this.nickName = nickName;
-  this.postDate = postDate;
-  this.text = text;
-  this.img = img;
-  this.likes = likes;
-  this.liked = false;
- }
-
- changeLike() {
-  this.liked = !this.liked;
-  if (this.liked) {
-   this.likes++;
-  } else {
-   this.likes--;
+  constructor({ id, userName, nickname, postDate, text, img, likes = 0 }) {
+    this.id = id || this.generateID();
+    this.userName = userName;
+    this.nickname = nickname;
+    this.postDate = postDate;
+    this.text = text;
+    this.img = img;
+    this.likes = likes;
+    this.liked = false;
   }
- }
 
- generateID() {
-  return Math.random().toString(32).substring(2, 9) + (+new Date).toString(32);
- }
-
- getDate() {
-  const options = {
-   year: 'numeric',
-   month: 'numeric',
-   day: 'numeric',
-   hour: '2-digit',
-   minute: '2-digit',
+  changeLike() {
+    this.liked = !this.liked;
+    if (this.liked) {
+      this.likes++;
+    } else {
+      this.likes--;
+    }
   }
-  return this.postDate.toLocaleString('ru-RU', options);
- }
+
+  generateID() {
+    return Math.random().toString(32).substring(2, 9) + (+new Date).toString(32);
+  }
+
+  getDate = () => {
+    const options = {
+      year: 'numeric',
+      month: 'numeric',
+      day: 'numeric',
+      hour: '2-digit',
+      minute: '2-digit',
+    }
+    return this.postDate.toLocaleString('ru-RU', options);
+  }
 }
 
 const twitter = new Twitter({
- listElem: '.tweet-list'
+  listElem: '.tweet-list'
 });
 
